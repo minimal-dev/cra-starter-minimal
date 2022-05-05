36bfba50e17581d08a45877a350d77a41519a023
Savelii Lukash <SaveliiLukash@gmail.com>
2022-05-05 13:51:55 +0300

    feat: add prettier to copy-bs-settings script

--- scripts/copy-bs-settings.js
+++ scripts/copy-bs-settings.js
@@ -1,13 +1,38 @@
-const fs = require('fs')
+/* eslint-disable import/no-extraneous-dependencies,no-console */
+const fs = require('fs').promises
+const prettier = require('prettier')
 
 const srcPath = 'node_modules/bootstrap/scss/_variables.scss'
 const destPath = 'src/styles/settings.scss'
 
-if (fs.existsSync(destPath)) {
-  console.log('Bootstrap: settings.scss already exists')
-} else {
-  fs.copyFile(srcPath, destPath, fs.constants.COPYFILE_EXCL, (err) => {
-    if (err) throw err
-    console.log('Bootstrap: settings.scss file copied from node_modules')
+// Test if file exists
+fs.access(destPath)
+  // File already exists
+  .then(() => console.log('Bootstrap: settings.scss already exists'))
+  // File does not exist
+  .catch(() => {
+    // Read the file
+    fs.readFile(srcPath, { encoding: 'utf-8' })
+      // Prettier
+      .then((data) =>
+        prettier
+          // Get prettier config
+          .resolveConfig(srcPath, {
+            config: '.prettierrc',
+          })
+          // Format the file
+          .then((options) =>
+            prettier.format(data, { ...options, filepath: srcPath })
+          )
+      )
+      // Write the file to the destination
+      .then((formattedData) =>
+        fs.writeFile(destPath, formattedData, { encoding: 'utf-8', flag: 'wx' })
+      )
+      // Log success
+      .then(() =>
+        console.log('Bootstrap: settings.scss file copied from node_modules')
+      )
+      // Log error
+      .catch((err) => console.error(err))
   })
-}
